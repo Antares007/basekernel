4142335d935d55ef08cb8439144aa6fabd052254
Archil Bolkvadze <a.bolkvadze@gmail.com>
2020-05-23 18:58:14 +0400

    contvert globals to locals

--- kernel/page.c
+++ kernel/page.c
@@ -15,28 +15,26 @@ static uint32_t pages_free = 0;
 static uint32_t pages_total = 0;
 
 static uint32_t *freemap = 0;
-static uint32_t freemap_bits = 0;
-static uint32_t freemap_bytes = 0;
 static uint32_t freemap_cells = 0;
-static uint32_t freemap_pages = 0;
-
-static void *main_memory_start = (void *)MAIN_MEMORY_START;
 
 #define CELL_BITS (8 * sizeof(*freemap))
 
 void page_init() {
   int i;
 
+  printf("total_memory: %d mb (%d kb)\n", total_memory,
+         total_memory * 1024 * 1024);
+
   pages_total = (total_memory * 1024 * 1024 - MAIN_MEMORY_START) / PAGE_SIZE;
   pages_free = pages_total;
   printf("memory: %d MB (%d KB) total\n", (pages_free * PAGE_SIZE) / MEGA,
          (pages_free * PAGE_SIZE) / KILO);
 
-  freemap = main_memory_start;
-  freemap_bits = pages_total;
-  freemap_bytes = 1 + freemap_bits / 8;
+  freemap = (void *)MAIN_MEMORY_START;
+  uint32_t freemap_bits = pages_total;
+  uint32_t freemap_bytes = 1 + freemap_bits / 8;
   freemap_cells = 1 + freemap_bits / CELL_BITS;
-  freemap_pages = 1 + freemap_bytes / PAGE_SIZE;
+  uint32_t freemap_pages = 1 + freemap_bytes / PAGE_SIZE;
 
   printf("memory: %d bits %d bytes %d cells %d pages\n", freemap_bits,
          freemap_bytes, freemap_cells, freemap_pages);
@@ -61,24 +59,19 @@ void page_stats(uint32_t *nfree, uint32_t *ntotal) {
 }
 
 void *page_alloc(bool zeroit) {
-  uint32_t i, j;
-  uint32_t cellmask;
-  uint32_t pagenumber;
-  void *pageaddr;
-
   if (!freemap) {
     printf("memory: not initialized yet!\n");
     return 0;
   }
-
-  for (i = 0; i < freemap_cells; i++) {
+  for (uint32_t i = 0; i < freemap_cells; i++) {
     if (freemap[i] != 0) {
-      for (j = 0; j < CELL_BITS; j++) {
-        cellmask = (1 << j);
+      for (uint32_t j = 0; j < CELL_BITS; j++) {
+        uint32_t cellmask = (1 << j);
         if (freemap[i] & cellmask) {
           freemap[i] &= ~cellmask;
-          pagenumber = i * CELL_BITS + j;
-          pageaddr = (pagenumber << PAGE_BITS) + main_memory_start;
+          uint32_t pagenumber = i * CELL_BITS + j;
+          void *pageaddr =
+              (pagenumber << PAGE_BITS) + (void *)MAIN_MEMORY_START;
           if (zeroit)
             memset(pageaddr, 0, PAGE_SIZE);
           pages_free--;
@@ -87,15 +80,13 @@ void *page_alloc(bool zeroit) {
       }
     }
   }
-
   printf("memory: WARNING: everything allocated\n");
   halt();
-
   return 0;
 }
 
 void page_free(void *pageaddr) {
-  uint32_t pagenumber = (pageaddr - main_memory_start) >> PAGE_BITS;
+  uint32_t pagenumber = (pageaddr - (void *)MAIN_MEMORY_START) >> PAGE_BITS;
   uint32_t cellnumber = pagenumber / CELL_BITS;
   uint32_t celloffset = pagenumber % CELL_BITS;
   uint32_t cellmask = (1 << celloffset);
